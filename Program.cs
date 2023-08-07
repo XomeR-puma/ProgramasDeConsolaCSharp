b7da982ae57372d8a71d95537ba39ace33eb8c3c
XomeR-puma <127904484+XomeR-puma@users.noreply.github.com>
2023-08-06 18:03:49 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Escribe el numero a multiplicar: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabla de multiplicar del numero {numero}: ");
+
+for (int i = 1;  i <= 15; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero}x{i}={resultado}");
+}
+
+Console.ReadKey();
